0379c8c6335aaa5cb4bd85af3f187f8a96a38cc6
Wocap93 <paco.gr.93@gmail.com>
2025-12-09 15:52:20 +0100

    footer.php

--- TFC/footer.php
+++ TFC/footer.php
@@ -1,4 +1,7 @@
 <footer>
     <p>© 2025 ACART - Sistema Furri | Desarrollado por Equipo 12</p>
-    <a href="proteccion_datos.php">Protección de Datos</a>
+
+    <?php if (isset($_SESSION['usuario'])): ?>
+        <a href="proteccion_datos.php">Protección de Datos</a>
+    <?php endif; ?>
 </footer>
