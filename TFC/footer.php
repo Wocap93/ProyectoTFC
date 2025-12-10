cb4afaf40bdfc5b4414d069505c877100a3e5f41
Wocap93 <paco.gr.93@gmail.com>
2025-12-10 22:05:08 +0100

    Delete TFC directory

--- TFC/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-    <p>© 2025 ACART - Sistema Furri | Desarrollado por Equipo 12</p>
-
-    <?php if (isset($_SESSION['usuario'])): ?>
-        <a href="proteccion_datos.php">Protección de Datos</a>
-    <?php endif; ?>
-</footer>
