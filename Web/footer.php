be328f67e9e6d9cbcb008a2ec516e7430d3d2aba
Wocap93 <paco.gr.93@gmail.com>
2025-12-11 19:37:57 +0100

    Update footer.php

--- Web/footer.php
+++ Web/footer.php
@@ -1,8 +1,9 @@
 <footer>
-    <p>© 2025 ACART - Sistema Furri | Desarrollado por Equipo 12</p>
+    <p>© 2025 ACART - Sistema Furri | Desarrollado por Equipo 3</p>
 
     <?php if (isset($_SESSION['usuario'])): ?>
         <a href="proteccion_datos.php">Protección de Datos</a>
     <?php endif; ?>
 </footer>
+
 
